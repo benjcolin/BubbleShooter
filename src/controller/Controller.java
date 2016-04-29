21d7ca8d8f2ce4c56beaaf834d309773c2473937
Stern <Benjamin.Stern@mgb.ch>
2016-04-29 07:57:49 +0200

    whole directory

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -19,66 +19,54 @@ public class Controller extends Application {
 	Preview preview = new Preview();
 	Parameter parameter = new Parameter();
 	Score score = new Score();
-	
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		view.start(primaryStage);
 		view.getInterceptS().valueProperty().addListener(new ChangeListener<Number>() {
-
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 				parameter.setIntercept(newValue.doubleValue());
 				preview.updatePreview(parameter.getIntercept(), parameter.getSlope());
-
 			}
 		});
 
 		view.getSlopeS().valueProperty().addListener(new ChangeListener<Number>() {
-		 
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 				parameter.setSlope(newValue.doubleValue());
 				preview.updatePreview(parameter.getIntercept(), parameter.getSlope());
-				
 			}
 		});
-		
-		
+
 		view.getAnchorpane().getChildren().add(preview.getLine());
 
 		Random rand = new Random();
 		for (int i = 0; i < 10; i++) {
 			// view.getGraphicscontext().strokeOval(rand.nextInt(800), y, w, h);
 			game.addBubble(new Bubble(new Circle(rand.nextInt(1000), rand.nextInt(1000), 10)));
-			game.getBubbles().get(i).getCircle().setOnMouseEntered(new EventHandler<MouseEvent> () {
-				
-				
-				
+			game.getBubbles().get(i).getCircle().setOnMouseEntered(new EventHandler<MouseEvent>() {
+
 				@Override
-				public void handle(final MouseEvent t){
-					
+				public void handle(final MouseEvent t) {
 					Circle circle = (Circle) t.getSource();
-					
-					 
-					view.getPosition().setText(circle.getCenterX()+", "+circle.getCenterY());
+					view.getPosition().setText(circle.getCenterX() + ", " + circle.getCenterY());
 				}
 			});
-			
-			game.getBubbles().get(i).getCircle().setOnMouseExited(new EventHandler<MouseEvent> (){
+
+			game.getBubbles().get(i).getCircle().setOnMouseExited(new EventHandler<MouseEvent>() {
 
 				@Override
 				public void handle(MouseEvent t) {
 					view.getPosition().setText("");
-					
+
 				}
-				
+
 			});
-			
-			
+
 			view.getAnchorpane().getChildren().add(game.getBubbles().get(i).getCircle());
 		}
-			
+
 		String scoreText = "" + score.getScore();
 		view.getScore().setText(scoreText);
 	}
